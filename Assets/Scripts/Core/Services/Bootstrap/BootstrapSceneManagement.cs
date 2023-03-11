02eda01b49dc605ab090a139784fa9f3db5c0b32
Roman Dunin <abromus@mail.ru>
2023-03-11 12:58:14 +0300

    Запуск с любой сцены

---
+++ Assets/Scripts/Core/Services/Bootstrap/BootstrapSceneManagement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor.SceneManagement;
+using UnityEditor;
+
+namespace Clicker.Core
+{
+    [InitializeOnLoad]
+    public class BootstrapSceneManagement
+    {
+        static BootstrapSceneManagement()
+        {
+            OnSceneListChanged();
+            EditorBuildSettings.sceneListChanged += OnSceneListChanged;
+        }
+
+        private static void OnSceneListChanged()
+        {
+            if (EditorBuildSettings.scenes.Length == 0)
+                return;
+
+            var startSceneIndex = 0;
+            var scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[startSceneIndex].path);
+
+            EditorSceneManager.playModeStartScene = scene;
+        }
+    }
+}
